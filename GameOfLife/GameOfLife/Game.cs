3ff00498ce0907c96b0d5396adb4ce30bd24c656
scoophamp <hampus_cousin@hotmail.com>
2016-03-04 11:08:10 +0100

    Added The Game Of Life - Lab

---
+++ GameOfLife/GameOfLife/Game.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Game
+    {
+        private int CountNeighbor(int x, int y, int[,] arrayer)
+        {
+
+            int count = 0;
+
+            try
+            {
+                if (arrayer[x - 1, y - 1] == 1)
+                {
+                    count++;
+
+                }
+
+            }
+            catch
+            {
+
+
+            }
+            try
+            {
+                if (arrayer[x, y - 1] == 1)
+
+                {
+                    count++;
+                }
+            }
+            catch
+            {
+
+
+            }
+
+            try
+            {
+                if (arrayer[y + 1, x] == 1)
+                {
+                    count++;
+                }
+            }
+            catch
+            {
+
+
+            }
+            try
+            {
+                if (arrayer[x, y + 1] == 1)
+                {
+                    count++;
+                }
+            }
+            catch
+            {
+
+            }
+            try
+            {
+                if (arrayer[x + 1, y + 1] == 1)
+                {
+                    count++;
+                }
+            }
+            catch
+            {
+
+
+            }
+            try
+            {
+                if (arrayer[x + 1, y] == 1)
+                {
+                    count++;
+                }
+            }
+            catch
+            {
+
+
+            }
+            try
+            {
+                if (arrayer[y - 1, x + 1] == 1)
+                {
+                    count++;
+                }
+            }
+            catch
+            {
+
+
+            }
+            try
+            {
+                if (arrayer[y - 1, x] == 1)
+                {
+                    count++;
+                }
+            }
+            catch
+            {
+
+
+            }
+
+            return count;
+        }
+
+
+        public void PrintVisual(int[,] arrayer)
+        {
+
+
+            for (int i = 0; i < arrayer.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayer.GetLength(1); j++)
+                {
+
+
+                    if (arrayer[i, j] == 0)
+                    {
+                        Console.Write(" ");
+
+
+                    }
+                    if (arrayer[i, j] > 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Write(" ");
+                        Console.BackgroundColor = ConsoleColor.Black;
+
+                    }
+                }
+                Console.WriteLine();
+            }
+        } //Method to write the cells out depending if live or not
+
+        public void ProcessGame(int[,] arrayer)
+        {
+            var SnapShot = new int[arrayer.GetLength(0), arrayer.GetLength(1)];
+
+            for (int x = 0; x < arrayer.GetLength(0); x++)
+            {
+                for (int y = 0; y < arrayer.GetLength(1); y++)
+                {
+                    SnapShot[x, y] = arrayer[x, y];
+                }
+            }
+            for (int x = 0; x < arrayer.GetLength(0); x++)
+            {
+                for (int y = 0; y < arrayer.GetLength(1); y++)
+                {
+                    if (arrayer[x, y] == 1 && CountNeighbor(x, y, SnapShot) < 2)
+                    {
+
+                        arrayer[x, y] = 0; //cell = dead
+                    }
+                    if (arrayer[x, y] == 1 && CountNeighbor(x, y, SnapShot) > 3)
+                    {
+                        arrayer[x, y] = 0; //cell = dead
+                    }
+                    if (arrayer[x, y] == 1 && CountNeighbor(x, y, SnapShot) == 2 || CountNeighbor(x, y, SnapShot) == 3)
+                    {
+                        arrayer[x, y] = 1; //cell = alive
+                    }
+                    if (arrayer[x, y] == 0 && CountNeighbor(x, y, SnapShot) == 3)
+                    {
+                        arrayer[x, y] = 1; //cell alive
+                    }
+
+                }
+
+
+
+            }
+
+
+        } //Method with an action to check neighboor and decides if cell lives on or not
+
+    }
+}
+
---
+++ GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            int[,] arr = new int[20, 20];
+
+            Random R = new Random();
+
+
+            for (int i = 0; i < 80; i++)
+            {
+                int x = R.Next(20);
+                int y = R.Next(20);
+                arr[x, y] = 1;
+            }
+            while (true)
+            {
+                Console.Clear();
+
+
+                game.PrintVisual(arr);
+                game.ProcessGame(arr);
+                System.Threading.Thread.Sleep(300);
+            }
+        }
+    }
+}
