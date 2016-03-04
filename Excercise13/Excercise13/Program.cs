33344c4d18371769bf683f65267a6f067c8d38da
scoophamp <hampus_cousin@hotmail.com>
2016-03-04 11:11:40 +0100

    13

---
+++ Excercise13/Excercise13/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("What year were you born: ");
+            var year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Month: ");
+            var month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Day: ");
+            var day = int.Parse(Console.ReadLine());
+
+
+            DateTime Birth = new DateTime(year, month, day);
+            var age = DateTime.Now.Year - year;
+            if (Birth.DayOfYear <= DateTime.Now.DayOfYear)
+            {
+                Console.WriteLine(age);
+            }
+            else
+            {
+                Console.WriteLine(age - 1);
+            }
+            
+            
+            DateTime mynext = Birth.AddYears(100);
+
+            DateTime today = DateTime.Today;
+
+            TimeSpan newwws = today - Birth;
+            
+            
+
+
+
+            Console.WriteLine("Hello " + name + ", you are " + age + " years old and you will turn" + newwws);
+            Console.WriteLine("Your 100th birtday will be " + mynext);
+            Console.ReadKey();
+
+
+            
+            
+
+
+            
+            
+        }
+    }
+}
