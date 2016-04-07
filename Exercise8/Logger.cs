03a0607db2f5e3026ad980eb5acd78861e8bd0e5
Hampus Cousin <hampus_cousin@hotmail.com>
2016-04-07 13:25:46 +0200

    Added files via upload

---
+++ Exercise8/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Logger
+    {
+        public List<string> Logpost { get;private set; }
+
+        public void Log(string message)
+        {
+            Logpost.Add(message);
+        }
+        
+        public Logger()
+        {
+            Logpost = new List<string>();
+
+            
+        }
+    }
+}
---
+++ Exercise8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myLogger = new Logger();
+            myLogger.Log("First post");
+            myLogger.Log("Second post");
+
+            foreach (var item in myLogger.Logpost)
+            {
+                Console.WriteLine(item);
+                Console.ReadKey();
+            }
+           
+        }
+    }
+}
